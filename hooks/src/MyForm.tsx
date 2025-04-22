97f4dedd88b7794071db6a07cc1d1d26cac4a70e
Steve Shilling <stevshil@users.noreply.github.com>
2025-04-22 21:57:47 +0100

    updated useForm hooks

--- hooks/src/MyForm.tsx
+++ hooks/src/MyForm.tsx
@@ -1,30 +1,33 @@
-import { Controller, useForm } from "react-hook-form";
-// import { TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 function MyForm() {
   const { register, handleSubmit, formState: {errors}, watch } = useForm();
 //   const { control } = useForm();
   const onSubmit = (data) => console.log(data);
   const firstName = watch("firstName", "");
+  const email = watch("email", "");
 
   return (
     <>
     {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-    <input {...register("firstName")} placeholder="First Name" />
-    <div>Current value: {firstName}</div>
-    <input {...register("lastName", {required: "Lastname is required"})} placeholder="Last Name" />
-    <br />{errors.lastName && <span>{errors.lastName.message}</span>}
-    {/* <Controller
-                name="email"
-                control={control}
-                render={({ field }) => (
-                    <TextField {...field} label="Email" />
-                )}
-            /> */}
-            <br/>
-            <button type="submit" onClick={handleSubmit(onSubmit)}>Submit</button>
+    <table><tbody>
+    <tr><td>Firstname:</td><td><input {...register("firstName")} placeholder="First Name" /></td></tr>
+    <tr><td colSpan="2">Current value: {firstName}</td></tr>
+    <tr><td>Lastname:</td><td><input {...register("lastName", {required: "Lastname is required"})} placeholder="Last Name" /></td></tr>
+    <tr><td colSpan="2">&nbsp;{errors.lastName && <span>{String(errors.lastName.message)}</span>}</td></tr>
+    <tr><td>Email:</td><td><input {...register("email", {required: "Email is required",
+      pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+        message: "Invalid email address"
+      }
+    })} placeholder="Email" /></td></tr>
+    <tr><td colSpan="2">&nbsp;{errors.email && <span>{String(errors.email.message)}</span>}</td></tr>
+    <br/>
+    <button type="submit" onClick={handleSubmit(onSubmit)}>Submit</button>
     {/* <br /><button type="submit">Submit</button> */}
     {/* </form> */}
+    </tbody>
+    </table>
     </>
   );
 }
